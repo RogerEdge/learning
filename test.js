87176b9b3afdf6579dc604349f6d3f6447f91299
DreandRde Edge <dreandrdeedge@DreandRdes-Air.lan>
2023-03-31 02:55:31 +0530

    my test file

---
+++ test.js
@@ -0,0 +1,24 @@
+function fibSeq(
+  num,
+  resultNum,
+  howManyTimes,
+  array
+) {
+  if ( resultNum > 1 ) {
+    array.push(num)
+  }
+  
+  const result = num + resultNum
+  
+  if ( howManyTimes > 0 ) {
+    console.log('howManyTimes', howManyTimes, num, '+', resultNum, '=',result)
+    howManyTimes = howManyTimes - 1 // --howManyTimes
+    return fibSeq(resultNum, result, howManyTimes, array)
+  }
+
+  return array
+}
+
+const result = fibSeq(1, 1, 20, [])
+
+console.log('result', result)
