4c6b5a2674084c8a4d4e4427ecc56d9d7315e1f3
DreandRde Edge <lilroger316@gmail.com>
2023-04-05 09:47:58 +0530

    memory managment

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //session storage
-const session={
-  code:'JTE7MC',
+const session = {
+  code: 'JTE7MC',
   user: undefined, // becomes populated on user long
   admin: undefined // becomes populated on admin long
 }
@@ -21,104 +21,127 @@ const session={
 app.post('/submit-form', (req, res, next) => {
   const formData = req.body;
   // Do something with the form data here
-  if(formData.code!=='031600'){
+  if (formData.code !== '031600') {
     const error = new Error('forbidden')
     error.status = 403;
     return next(error);
   }
   //const code = generateCode();
   const code = 'JTE7MC';
-  session.code=code
-  res.json({code: code});
+  session.code = code
+  res.json({ code: code });
 });
 
 app.post('/push-user', (req, res, next) => {
   const formData = req.body;
-  
+
   // reject any invalid requests
-  const valid=isCodeValid(formData.code);
-  if(!valid){
+  const valid = isCodeValid(formData.code);
+  if (!valid) {
     const error = new Error('forbidden')
     error.status = 403;
     return next(error);
   }
 
-  console.log('/////')
+  console.log('///// guard code')
+
 
   // only admin can push user data
-  if(formData.isAdmin===1){
+  if (formData.isAdmin === 1) {
     // prevent bad requests
-    if(!formData.userSession){
+    if (!formData.userSession) {
       console.warn('🟠 no user session')
       return
     }
 
-    session.admin.userSession = session.admin.userSession || {}
-    session.admin.userSession=mergeObjects(session.admin.userSession,formData.userSession)
+    /*session.admin.userSession = session.admin.userSession || {}
+    session.admin.userSession = mergeObjects(session.admin.userSession, formData.userSession)
+
+    const noDate = !formData.userSession.lastUpdatedAt && session.user.lastUpdatedAt
+    const outDated = formData.userSession.lastUpdatedAt<session.user.lastUpdatedAt
+
+    console.log('noDate', noDate)
+    console.log('outDated', outDated)
+    console.log('GGGGG',formData.userSession.lastUpdatedAt-session.user.lastUpdatedAt )
+
+    if (noDate || outDated) {
+      const error = new Error('out of date update')
+      error.status = 400;
+      return next(error);
+
+    }*/
 
 
     // merge existing user data with formData
-    session.user=mergeObjects(session.user, formData.userSession)
-    session.user.lastUpdatedAt=Date.now()
-    console.log('session----------------', session.user.tictactoe)
-    console.log('session----------------2', session.admin.userSession.tictactoe)
+    session.user = mergeObjects(session.user, formData.userSession)
+    session.user.lastUpdatedAt = Date.now()
+  } else{
+    session.user = mergeObjects(session.user, formData)
+    session.user.lastUpdatedAt = Date.now()
   }
-})  
+
+  console.info('pushed data',session.user)
+})
 
 
 app.post('/repost', (req, res, next) => {
   let formData = req.body;
-  const valid=isCodeValid(formData.code);
-  if(!valid){
+  const valid = isCodeValid(formData.code);
+  if (!valid) {
     const error = new Error('forbidden')
     error.status = 403;
     return next(error);
   }
 
-  if(formData.isAdmin===1){
-    session.admin=formData
-    console.log('cccccccsession.admin', session.admin.userSession.tictactoe)
+  session.user = session.user || {}
+
+  if (formData.isAdmin === 1) {
+    session.admin = formData
     //the admin always gets the users data
     res.json(session.user);
-  }else{
-    session.user=session.user || formData
-    
-    if(session.user.lastUpdatedAt){
-      if(!formData.lastUpdatedAt || session.user.lastUpdatedAt > formData.lastUpdatedAt){
-        formData=mergeObjects(formData, session.user)
+  } else {
+    res.json(session.user);
+
+    //we decided not to update info here only read
+    /*
+    session.user = session.user || formData
+
+    if (session.user.lastUpdatedAt) {
+      if (!formData.lastUpdatedAt || session.user.lastUpdatedAt > formData.lastUpdatedAt) {
+        formData = mergeObjects(formData, session.user)
         console.warn('🟠 server session was newer than user session')
       }
     }
-    session.user=mergeObjects(session.user,formData)
+    session.user = mergeObjects(session.user, formData)
 
-    session.user.lastUpdatedAt=Date.now()
-    res.json(session.user);
+    session.user.lastUpdatedAt = Date.now()
+    res.json(session.user);*/
   }
 });
 
 app.post('/submit-nsync', (req, res, next) => {
   const formData = req.body
-  const valid=isCodeValid(formData.code);
+  const valid = isCodeValid(formData.code);
   // Do something with the form data here
-  if(!valid){
-    console.warn('incorrect code expected ', session.code,'but got',formData.code)
+  if (!valid) {
+    console.warn('incorrect code expected ', session.code, 'but got', formData.code)
     const error = new Error('incorrect code')
     error.status = 403;
     return next(error);
   }
-  session.status= 'ok'
+  session.status = 'ok'
   res.send('ok');
 });
 
 // check if user has entered code yet
 app.get('/check-user-status', (req, res, next) => {
-  if(session.status=='ok'){
+  if (session.status == 'ok') {
     res.send(session.status);
-    return 
+    return
   }
   const error = new Error('not ready')
-    error.status = 401;
-    return next(error);
+  error.status = 401;
+  return next(error);
 });
 
 // Define a default route
@@ -134,43 +157,43 @@ app.listen(port, '0.0.0.0', () => {
 function generateCode() {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let code = '';
-  
+
   for (let i = 0; i < 6; i++) {
     const randomIndex = Math.floor(Math.random() * chars.length);
     const randomChar = chars[randomIndex];
     code += randomChar;
   }
-  
+
   return code.toUpperCase();
 }
 
 function isCodeValid(code) {
-  return code && code.toUpperCase()==session.code
-} 
+  return code && code.toUpperCase() == session.code
+}
 
 
 function mergeObjects(obj1, obj2) {
-	const result = {};
+  const result = {};
 
-	for (let key in obj1) {
-		if (Array.isArray(obj1[key])) {
+  for (let key in obj1) {
+    if (Array.isArray(obj1[key])) {
       result[key] = obj1[key];
-      if(Array.isArray(obj2[key])){
+      if (Array.isArray(obj2[key])) {
         result[key] = obj2[key];
-        
+
       }
     } else if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
-			result[key] = mergeObjects(obj1[key], obj2[key]);
-		} else {
-			result[key] = obj1[key];
-		}
-	}
+      result[key] = mergeObjects(obj1[key], obj2[key]);
+    } else {
+      result[key] = obj1[key];
+    }
+  }
 
-	for (let key in obj2) {
-		//if (!result.hasOwnProperty(key)) {
-			result[key] = obj2[key];
-		//}
-	}
+  for (let key in obj2) {
+    //if (!result.hasOwnProperty(key)) {
+    result[key] = obj2[key];
+    //}
+  }
 
-	return result;
+  return result;
 }
