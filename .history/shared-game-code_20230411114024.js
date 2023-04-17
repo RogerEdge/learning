b7eec1746f8d4c31b15128f331d74863dc63327e
Acker Apple <y>
2023-04-17 11:44:27 -0400

    release

---
+++ .history/shared-game-code_20230411114024.js
@@ -0,0 +1,90 @@
+let gameOver = false
+let player1Emoji = '⚫️'
+let player2Emoji = '🔴'
+let currentPlayer = player1Emoji;
+
+function setCurrentPlayer(player) {
+  currentPlayer = player;
+}
+
+// Create a new Audio object and set the source to your audio file
+const moveAudio = new Audio("https://assets.mixkit.co/active_storage/sfx/2894/2894-preview.mp3");
+function playMoveSound() {
+  const moveAudio = new Audio("https://assets.mixkit.co/active_storage/sfx/2894/2894-preview.mp3");
+
+  // Play the audio
+  moveAudio.play();
+
+}
+
+// update display and play a sound
+function claimBoardSpace(space, currentPlayer) {
+  markBoardSpace(space, currentPlayer)
+  playMoveSound()
+}
+
+// called just to update display only (no sound)
+function markBoardSpace(space, currentPlayer) {
+  document.getElementById(space).innerHTML = currentPlayer;
+}
+
+function onClick(id, boardMemory, gameSession, checkForWinner) {
+  //If the game is over, prevent anymore code from running
+  if (gameOver) {
+    return
+  }
+
+  if (!boardMemory[id]) {
+    //mark the memory space with the current player
+    boardMemory[id] = currentPlayer;
+    
+    // claim the space with the current players emoji and play sound
+    claimBoardSpace(id, currentPlayer)
+    
+    const isPlayerOne = currentPlayer === player1Emoji
+    const isGameOver = checkForWinner(gameSession);
+    if (isGameOver) {
+      gameOver = true
+      const winningPlayer = isPlayerOne ? 1 : 2
+
+      gameWon(gameSession, winningPlayer)
+
+      setTimeout(() => {
+        alert('✅ player ' + winningPlayer + ' won')
+      }, 10);
+    }
+
+    //change players
+    currentPlayer = isPlayerOne ? player2Emoji : player1Emoji;
+  }
+}
+// Create a new Audio object and set the source to your audio file
+const winAudio = new Audio("https://cdn.pixabay.com/audio/2021/08/09/audio_ff81343224.mp3");
+function playWinSound() {
+
+  // Play the audio
+  winAudio.play();
+
+}
+
+function gameWon(gameSession, winningPlayer) {
+  gameSession.winningPlayer = winningPlayer
+  playWinSound()
+  const body = document.getElementsByTagName('body')[0]
+  const content = `<div class="gif-overlay">
+  <img class="gif-win" src="https://i.pinimg.com/originals/cf/50/6d/cf506d6998d68de01e9171f30fc4e287.gif" alt="Animated GIF">
+  </div>
+`
+  const newElement = document.createElement('div')
+  newElement.innerHTML = content
+
+  body.appendChild(newElement)
+
+  setTimeout(() => {
+    body.removeChild(newElement)
+  }, 10000);
+}
+//preload winning image
+const imgUrl = 'https://i.pinimg.com/originals/cf/50/6d/cf506d6998d68de01e9171f30fc4e287.gif';
+const img = new Image();
+img.src = imgUrl;
---
+++ .history/src/Mentorship_20230415103336.js
@@ -0,0 +1,56 @@
+import './Mentorship.css'
+
+export function Mentorship() {
+  const opacity60 = { opacity: '60%' }
+
+  const text2x = {
+    fontSize: '2rem'
+  }
+
+  const buttonLink = {
+    textDecoration: 'none', color: '#000'
+  }
+
+  return (
+    <div>
+      <div className="center">
+        <h2>Mentorship by Acker Apple until 2029</h2>
+        <div class="centeredContent">
+          <div>
+            <img src="assets/profile_photo_low.jpg" alt="Roger img" className="img profileImage" />
+            <div>Roger Edge</div>
+          </div>
+          <span className="largePlus">+</span>
+          <div>
+            <img src="https://media.licdn.com/dms/image/C4E03AQGexv4peFAy9Q/profile-displayphoto-shrink_800_800/0/1560199522202?e=1686182400&amp;v=beta&amp;t=xyCzv59XdeSMTg57f4kKbw7szs4trgetn1ncemVSElc" alt="Acker img" className="img profileImage" />
+            <div>Acker Apple</div>
+          </div>
+        </div>
+      </div>
+
+      <p>
+        Roger Edge III, is being mentored by the esteemed Acker Dawn Apple, an accomplished software engineer and expert in full-stack development. With their extensive experience and mastery of NodeJs, TypeScript, MongoDB, Jira, Slack, React, HTML, CSS, JavaScript, and other front-end and back-end technologies, Acker has a proven track record of successfully guiding individuals in achieving their professional goals. This partnership, which will extend until 2029, is designed to ensure that Roger attains a thriving career in full-stack development.
+      </p>
+      <p>
+        Acker Apple recognizes Roger's immense potential, which is why he has committed to providing personalized guidance and support throughout the mentorship. By sharing their extensive knowledge and insights gained from years of experience in the field, Acker is equipping Roger with the necessary tools to navigate the ever-evolving world of software development.
+      </p>
+      <p>
+        Under Acker's tutelage, Roger is not only learning the most in-demand technologies, but also mastering essential soft skills, such as effective communication, teamwork, and time management.
+      </p>
+      <p>
+        The mentorship program is structured to provide Roger with comprehensive exposure to the different aspects of full-stack development. From concept creation to the final deployment of a project, Acker ensures that Roger is well-versed in every stage of the development process. In addition, Acker has designed a curriculum that caters to Roger's learning style and aptitude, allowing them to make steady progress and consistently refine their skillset.
+      </p>
+      <hr style={opacity60} />
+      <br />
+      <div className="flex flex-wrap">
+        <button style={text2x} className='flex1'>
+          <a style={buttonLink} href="https://ackerapple.com">Acker Website</a>
+        </button>
+        <button style={text2x} className='flex1'>
+          <a style={buttonLink} href="https://www.linkedin.com/in/acker-apple/">Acker Linkedin</a>
+        </button>
+      </div>
+    </div>
+  )
+}
+
